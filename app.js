5b5bb5b6d6c3ef8305a1f6a81a1735614f67bd1d
Carlos Villalvazo <zentrix.24@gmail.com>
2020-10-24 21:20:09 +0000

    add submit functionality

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ new Vue({
             eventDate: 'August 14th - 16th', //creation of properties
             eventTitle: 'Summer Festival!', 
             eventDescription: "It's back! This years summer festival will be in the beautiful countryside featuring our best line up ever!"
+        },
+        newNameText: '',
+        guestName: []
+    },
+    methods:{
+        formSumitted: function(){
+            this.guestName.push(this.newNameText)
+            this.newNameText = ''
         }
-    } 
+    }
 });
