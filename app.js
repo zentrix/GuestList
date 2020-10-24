082a6c914e37e72fc0f0593e3e7b739d78bc91f6
Carlos Villalvazo <zentrix.24@gmail.com>
2020-10-24 20:45:26 +0000

    add vuejs to the project

---
+++ app.js
@@ -0,0 +1,10 @@
+new Vue({
+    el: "#app", 
+    data: {
+        event: { // creation of event 
+            eventDate: 'August 14th - 16th', //creation of properties
+            eventTitle: 'Summer Festival!', 
+            eventDescription: "It's back! This years summer festival will be in the beautiful countryside featuring our best line up ever!"
+        }
+    } 
+});
