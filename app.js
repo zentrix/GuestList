dbf70c718d4b2d53da7f68540fd7a36cfde682b4
Carlos Villalvazo <zentrix.24@gmail.com>
2020-10-25 19:32:34 +0000

    add progress bar for the guestNames

--- app.js
+++ app.js
@@ -7,14 +7,30 @@ new Vue({
             eventDescription: "It's back! This years summer festival will be in the beautiful countryside featuring our best line up ever!"
         },
         newNameText: '',
-        guestName: [],
-        formSumitClass: ""
+        guestName: [], 
+        appStyles: {
+            marginTop: '25px'
+        },
+        eventCapacity: 25, 
+        eventCapacityPercentage: 0
     },
     methods:{
         formSumitted: function(){ //creation of function 
-            this.guestName.push(this.newNameText)
-            this.newNameText = '',
-            this.formSumitClass = "submitted"
+            console.log("method")
+            if(this.newNameText .length > 0 && this.eventCapacityPercentage < 100) {
+                this.guestName.push(this.newNameText)
+                this.newNameText = '',
+                this.eventCapacityPercentage = this.guestName.length / (this.eventCapacity/100)
+            }
+        }, 
+        methodCall: function(){
+            this.appStyles.marginTop = "50px"
+        }
+    },
+    computed:{ //computed properties
+        sortName: function(){ //function to sort the guestNames
+            console.log("computed")
+            return this.guestName.sort()
         }
     }
 });
